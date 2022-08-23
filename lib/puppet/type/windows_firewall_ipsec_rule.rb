006cf7d68352243ff7b9330aa826a584efaa4411
Alexandre Jardon <Alexandre.Jardon@ingenico.com>
2022-08-23 11:59:26 +0200

    Code cleaning

--- lib/puppet/type/windows_firewall_ipsec_rule.rb
+++ lib/puppet/type/windows_firewall_ipsec_rule.rb
@@ -1,7 +1,7 @@
 require 'puppet/parameter/boolean'
 
 Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
-  @doc = "Manage Windows Firewall with Puppet"
+  @doc = 'Windows Firewall with Puppet'
 
   ensurable do
     desc "How to ensure this firewall rule (`present` or `absent`)"
@@ -33,7 +33,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:display_name) do
-    desc "Specifies the localized, user-facing name of the firewall rule being created"
+    desc 'Specifies the localized, user-facing name of the firewall rule being created'
     defaultto { @resource[:name] }
     validate do |value|
       unless value.kind_of?(String)
@@ -43,7 +43,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:description) do
-    desc "This parameter provides information about the firewall rule"
+    desc 'This parameter provides information about the firewall rule'
     defaultto ''
     validate do |value|
       unless value.kind_of?(String)
@@ -53,7 +53,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:profile, :array_matching=>:all) do
-    desc "Specifies one or more profiles to which the rule is assigned"
+    desc 'Specifies one or more profiles to which the rule is assigned'
     newvalues(:domain, :private, :public, :any)
 
     # Thanks Gary!
@@ -64,14 +64,14 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:display_group) do
-    desc "This parameter specifies the source string for the DisplayGroup parameter (read-only)"
+    desc 'This parameter specifies the source string for the DisplayGroup parameter (read-only)'
     validate do |value|
-      fail("grouping is readonly: https://social.technet.microsoft.com/Forums/office/en-US/669a8eaf-13d1-4010-b2ac-30c800c4b152/2008r2-firewall-add-rules-to-group-create-new-group")
+      fail('grouping is readonly: https://social.technet.microsoft.com/Forums/office/en-US/669a8eaf-13d1-4010-b2ac-30c800c4b152/2008r2-firewall-add-rules-to-group-create-new-group')
     end
   end
 
   newproperty(:local_address) do
-    desc "Specifies that network packets with matching IP addresses match this rule (hostname not allowed)"
+    desc 'Specifies that network packets with matching IP addresses match this rule (hostname not allowed)'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -80,7 +80,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:remote_address) do
-    desc "Specifies that network packets with matching IP addresses match this rule (hostname not allowed)"
+    desc 'Specifies that network packets with matching IP addresses match this rule (hostname not allowed)'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -89,7 +89,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:protocol) do
-    desc "This parameter specifies the protocol for an IPsec rule"
+    desc 'This parameter specifies the protocol for an IPsec rule'
     # Also accept 0-255 :/
     newvalues(:tcp, :udp, :icmpv4, :icmpv6, /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)
     isrequired
@@ -100,7 +100,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
 
 
   newproperty(:local_port) do
-    desc "Specifies that network packets with matching IP port numbers match this rule"
+    desc 'Specifies that network packets with matching IP port numbers match this rule'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -109,7 +109,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:remote_port) do
-    desc "This parameter value is the second end point of an IPsec rule"
+    desc 'This parameter value is the second end point of an IPsec rule'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -118,7 +118,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:mode) do
-    desc "Specifies the type of IPsec mode connection that the IPsec rule defines (None, Transport, or Tunnel)"
+    desc 'Specifies the type of IPsec mode connection that the IPsec rule defines (None, Transport, or Tunnel)'
     newvalues(:none, :transport, :tunnel)
 
     defaultto :transport
@@ -126,7 +126,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
 
 
   newproperty(:interface_type, :array_matching=>:all) do
-    desc "Specifies that only network connections made through the indicated interface types are subject to the requirements of this rule"
+    desc 'Specifies that only network connections made through the indicated interface types are subject to the requirements of this rule'
     newvalues(:any, :wired, :wireless, :remote_access)
 
     defaultto :any
@@ -137,21 +137,21 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:inbound_security) do
-    desc "This parameter determines the degree of enforcement for security on inbound traffic"
+    desc 'This parameter determines the degree of enforcement for security on inbound traffic'
     newvalues(:none, :require, :request)
 
     defaultto :none
   end
 
   newproperty(:outbound_security) do
-    desc "This parameter determines the degree of enforcement for security on outbound traffic"
+    desc 'This parameter determines the degree of enforcement for security on outbound traffic'
     newvalues(:none, :require, :request)
 
     defaultto :none
   end
 
   newproperty(:phase1auth_set) do
-    desc "Gets the main mode rules that are associated with the given phase 1 authentication set to be created"
+    desc 'Gets the main mode rules that are associated with the given phase 1 authentication set to be created'
     newvalues(:none, :default, :computerkerberos, :anonymous)
 
     defaultto do
@@ -164,7 +164,7 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newproperty(:phase2auth_set) do
-    desc "Gets the IPsec rules that are associated with the given phase 2 authentication set to be created"
+    desc 'Gets the IPsec rules that are associated with the given phase 2 authentication set to be created'
     newvalues(:none, :default, :userkerberos)
 
     defaultto do
@@ -177,10 +177,10 @@ Puppet::Type.newtype(:windows_firewall_ipsec_rule) do
   end
 
   newparam(:name) do
-    desc "Name of this rule"
+    desc 'Name of this rule'
     isnamevar
     validate do |value|
-      fail("it is not allowed to have a rule called 'any'") if value.downcase == "any"
+      fail("it is not allowed to have a rule called 'any'") if value.downcase == 'any'
     end
   end
 
--- lib/puppet/type/windows_firewall_rule.rb
+++ lib/puppet/type/windows_firewall_rule.rb
@@ -1,7 +1,7 @@
 require 'puppet/parameter/boolean'
 
 Puppet::Type.newtype(:windows_firewall_rule) do
-  @doc = "Manage Windows Firewall with Puppet"
+  @doc = 'Manage Windows Firewall with Puppet'
 
   ensurable do
     desc "How to ensure this firewall rule (`present` or `absent`)"
@@ -35,7 +35,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:display_name) do
-    desc "Display name for this rule"
+    desc 'Display name for this rule'
     defaultto { @resource[:name] }
     validate do |value|
       unless value.kind_of?(String)
@@ -45,7 +45,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:description) do
-    desc "Description of this rule"
+    desc 'Description of this rule'
     defaultto ''
     validate do |value|
       unless value.kind_of?(String)
@@ -69,7 +69,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:profile, :array_matching=>:all) do
-    desc "Which profile(s) this rule belongs to, use an array to pass more then one"
+    desc 'Which profile(s) this rule belongs to, use an array to pass more then one'
     newvalues(:domain, :private, :public, :any)
 
     # Thanks Gary!
@@ -81,14 +81,14 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:display_group) do
-    desc "group that the rule belongs to (read-only)"
+    desc 'group that the rule belongs to (read-only)'
     validate do |value|
-      fail("grouping is readonly: https://social.technet.microsoft.com/Forums/office/en-US/669a8eaf-13d1-4010-b2ac-30c800c4b152/2008r2-firewall-add-rules-to-group-create-new-group")
+      fail('grouping is readonly: https://social.technet.microsoft.com/Forums/office/en-US/669a8eaf-13d1-4010-b2ac-30c800c4b152/2008r2-firewall-add-rules-to-group-create-new-group')
     end
   end
 
   newproperty(:local_address) do
-    desc "the local IP the rule targets (hostname not allowed)"
+    desc 'the local IP the rule targets (hostname not allowed)'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -98,7 +98,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:remote_address) do
-    desc "the remote IP the rule targets (hostname not allowed)"
+    desc 'the remote IP the rule targets (hostname not allowed)'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -108,7 +108,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:protocol) do
-    desc "the protocol the rule targets"
+    desc 'the protocol the rule targets'
     # Also accept 0-255 :/
     newvalues(:tcp, :udp, :icmpv4, :icmpv6, /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)
     isrequired
@@ -135,7 +135,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:local_port) do
-    desc "the local port the rule targets"
+    desc 'the local port the rule targets'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -151,7 +151,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:remote_port) do
-    desc "the remote port the rule targets"
+    desc 'the remote port the rule targets'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -161,20 +161,20 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:edge_traversal_policy) do
-    desc "Apply rule to encapsulated traffic (?) - see: https://serverfault.com/questions/89824/windows-advanced-firewall-what-does-edge-traversal-mean#89846"
+    desc 'Apply rule to encapsulated traffic (?) - see: https://serverfault.com/questions/89824/windows-advanced-firewall-what-does-edge-traversal-mean#89846'
     newvalues(:block, :allow, :defer_to_user, :defer_to_app)
 
     defaultto :block
   end
 
   newproperty(:action) do
-    desc "What to do when this rule matches (Accept/Reject)"
+    desc 'What to do when this rule matches (Accept/Reject)'
     newvalues(:block, :allow)
     isrequired
   end
 
   newproperty(:program) do
-    desc "Path to program this rule applies to"
+    desc 'Path to program this rule applies to'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -184,7 +184,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:interface_type, :array_matching=>:all) do
-    desc "Interface types this rule applies to"
+    desc 'Interface types this rule applies to'
     newvalues(:any, :wired, :wireless, :remote_access)
 
     defaultto :any
@@ -195,7 +195,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:service) do
-    desc "service names this rule applies to"
+    desc 'service names this rule applies to'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -205,19 +205,19 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:authentication) do
-    desc "Specifies that authentication or encryption is required on firewall rules (authentication, encryption)"
+    desc 'Specifies that authentication or encryption is required on firewall rules (authentication, encryption)'
     newvalues(:notrequired, :required, :noencap)
     defaultto :notrequired
   end
 
   newproperty(:encryption) do
-    desc "Specifies that authentication or encryption is required on firewall rules (authentication, encryption)"
+    desc 'Specifies that authentication or encryption is required on firewall rules (authentication, encryption)'
     newvalues(:notrequired, :required, :dynamic)
     defaultto :notrequired
   end
 
   newproperty(:remote_machine) do
-    desc "Specifies that matching IPsec rules of the indicated computer accounts are created"
+    desc 'Specifies that matching IPsec rules of the indicated computer accounts are created'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -227,7 +227,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:local_user) do
-    desc "Specifies that matching IPsec rules of the indicated user accounts are created"
+    desc 'Specifies that matching IPsec rules of the indicated user accounts are created'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -237,7 +237,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newproperty(:remote_user) do
-    desc "Specifies that matching IPsec rules of the indicated user accounts are created"
+    desc 'Specifies that matching IPsec rules of the indicated user accounts are created'
 
     def insync?(is)
       "#{is}".downcase == "#{should}".downcase
@@ -247,7 +247,7 @@ Puppet::Type.newtype(:windows_firewall_rule) do
   end
 
   newparam(:name) do
-    desc "Name of this rule"
+    desc 'Name of this rule'
     isnamevar
     validate do |value|
       fail("it is not allowed to have a rule called 'any'") if value.downcase == "any"
